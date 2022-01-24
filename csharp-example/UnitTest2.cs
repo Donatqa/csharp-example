ee8d68900ca572cec9f8fdca21b196becde1096c
Donat Avkhadiev <sir.mailwork@gmail.com>
2022-01-23 20:20:20 -0500

    login into admin panel

---
+++ csharp-example/UnitTest2.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace csharp_example
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        public class MyFirstTest
+        {
+            private IWebDriver driver;
+
+            [SetUp]
+            public void Start()
+            {
+                driver = new ChromeDriver();
+            }
+
+            [Test]
+            public void FirstTest()
+            {
+                driver.Navigate().GoToUrl("http://localhost/litecart/admin/");
+                driver.FindElement(By.XPath("//*[@name='username']")).SendKeys("admin");
+                driver.FindElement(By.XPath("//*[@name='password']")).SendKeys("admin");
+                driver.FindElement(By.XPath("//*[@name='login']")).Click();
+            }
+
+            [TearDown]
+            public void stop()
+            {
+                driver.Quit();
+                driver = null;
+
+
+            }
+        }
+    }
+}
